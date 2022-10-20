ae823d15ab3a77ea71b3dd9d34df43cc37eb2d9e
«Ilya <333zero777@mail.ru>
2022-10-20 19:44:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+try
+{
+
+}
+catch (ArgumentOutOfRangeException ex) { Console.WriteLine(ex); }
+catch (IndexOutOfRangeException ex) { Console.WriteLine(ex); }
+catch (StackOverflowException ex) { Console.WriteLine(ex); }
+catch (OutOfMemoryException ex) { Console.WriteLine(ex); }
+catch (MyException ex) { Console.WriteLine(ex); }
+
+
+public class MyException : Exception
+{
+    public MyException() : base()
+    {
+        Console.WriteLine("Общая ошибка");
+    }
+
+}
